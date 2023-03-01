aa1e4b2a1b847dacb2851f4583e3c817e5d0552c
talia.tsameret <talia.tsameret@bottomline.com>
2023-03-01 12:06:47 +0200

    add tailwind navBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import NavBar from  '../components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      Hi there!
+    <div>
+      <NavBar/>
+      </div>
+      <div>
+        <h1>Hello</h1>
+        </div>
     </>
   )
 }
